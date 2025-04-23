f04ac3950b4c9030e538f48323190f5e729c3f93
ZhilanMuhammad <zhilanmuhammad11@gmail.com>
2025-04-23 14:27:26 +0700

    💄 update slicing

--- resources/views/page/footer.blade.php
+++ resources/views/page/footer.blade.php
@@ -5,16 +5,14 @@
                 <div class="single-footer-widget">
                     <h6>Tentang Kami</h6>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-                        ut labore dolore
-                        magna aliqua.
+                        Roditri Mobil adalah platform jual beli mobil bekas terpercaya.
                     </p>
                 </div>
             </div>
             <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget mail-chimp">
                     <h6 class="mb-20">Instragram Feed</h6>
-                    <ul class="instafeed d-flex flex-wrap">
+                    <!-- <ul class="instafeed d-flex flex-wrap">
                         <li><img src="{{ asset('themes/img/i1.jpg') }}" alt=""></li>
                         <li><img src="{{ asset('themes/img/i2.jpg') }}" alt=""></li>
                         <li><img src="{{ asset('themes/img/i3.jpg') }}" alt=""></li>
@@ -23,7 +21,7 @@
                         <li><img src="{{ asset('themes/img/i6.jpg') }}" alt=""></li>
                         <li><img src="{{ asset('themes/img/i7.jpg') }}" alt=""></li>
                         <li><img src="{{ asset('themes/img/i8.jpg') }}" alt=""></li>
-                    </ul>
+                    </ul> -->
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-6">
@@ -33,8 +31,7 @@
                     <div class="footer-social d-flex align-items-center">
                         <a href="#"><i class="fa fa-facebook"></i></a>
                         <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-                        <a href="#"><i class="fa fa-behance"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
                     </div>
                 </div>
             </div>
@@ -43,8 +40,9 @@
             <p class="footer-text m-0">
                 Copyright &copy;
                 <script>
-                    document.write(new Date().getFullYear());
-                </script> All rights reserved | This template is made with by <a href="https://colorlib.com" target="_blank">RoditriMobil</a>
+                document.write(new Date().getFullYear());
+                </script> All rights reserved | This template is made with by <a href="https://colorlib.com"
+                    target="_blank">RoditriMobil</a>
             </p>
         </div>
     </div>
--- resources/views/page/tentang/index.blade.php
+++ resources/views/page/tentang/index.blade.php
@@ -1,102 +1,72 @@
 @extends('page.main')
 @section('content')
-    <section class="banner-area organic-breadcrumb">
-        <div class="container">
-            <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
-                <div class="col-first">
-                    <h1>Tentang Kami</h1>
-                    <nav class="d-flex align-items-center">
-                        <a href="{{ route('home') }}">Beranda<span class="lnr lnr-arrow-right"></span></a>
-                        <a href="{{ route('tentang') }}">Tentang</a>
-                    </nav>
-                </div>
+<section class="banner-area organic-breadcrumb">
+    <div class="container">
+        <div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
+            <div class="col-first">
+                <h1>Tentang Kami</h1>
+                <nav class="d-flex align-items-center">
+                    <a href="{{ route('home') }}">Beranda<span class="lnr lnr-arrow-right"></span></a>
+                    <a href="{{ route('tentang') }}">Tentang</a>
+                </nav>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="blog_area single-post-area section_gap">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 posts-list">
-                    <div class="single-post row">
-                        <div class="col-lg-12">
-                            <div class="feature-img">
-                                <img class="img-fluid" src="{{ asset('themes/img/blog/feature-img1.jpg') }}" alt="">
-                            </div>
-                        </div>
-                        <div class="col-lg-3  col-md-3">
-                            <div class="blog_info text-right">
-                                <div class="post_tag">
-                                    <a href="#">Food,</a>
-                                    <a class="active" href="#">Technology,</a>
-                                    <a href="#">Politics,</a>
-                                    <a href="#">Lifestyle</a>
-                                </div>
-                                <ul class="blog_meta list">
-                                    <li><a href="#">Mark wiens<i class="lnr lnr-user"></i></a></li>
-                                    <li><a href="#">12 Dec, 2018<i class="lnr lnr-calendar-full"></i></a></li>
-                                    <li><a href="#">1.2M Views<i class="lnr lnr-eye"></i></a></li>
-                                    <li><a href="#">06 Comments<i class="lnr lnr-bubble"></i></a></li>
-                                </ul>
-                                <ul class="social-links">
-                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-github"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-lg-9 col-md-9 blog_details">
-                            <h2>Astronomy Binoculars A Great Alternative</h2>
-                            <p class="excert">
-                                MCSE boot camps have its supporters and its detractors. Some people do not understand
-                                why you should have to spend money on boot camp when you can get the MCSE study
-                                materials yourself at a fraction.
-                            </p>
-                            <p>
-                                Boot camps have its supporters and its detractors. Some people do not understand why
-                                you should have to spend money on boot camp when you can get the MCSE study materials
-                                yourself at a fraction of the camp price. However, who has the willpower to actually
-                                sit through a self-imposed MCSE training. who has the willpower to actually sit through
-                                a self-imposed
-                            </p>
-                            <p>
-                                Boot camps have its supporters and its detractors. Some people do not understand why
-                                you should have to spend money on boot camp when you can get the MCSE study materials
-                                yourself at a fraction of the camp price. However, who has the willpower to actually
-                                sit through a self-imposed MCSE training. who has the willpower to actually sit through
-                                a self-imposed
-                            </p>
+<section class="blog_area single-post-area section_gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 posts-list">
+                <div class="single-post row">
+                    <div class="col-lg-12">
+                        <div class="feature-img">
+                            <img class="img-fluid" src="{{ asset('themes/img/blog/car-11.jpg') }}" alt="">
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="blog_right_sidebar">
 
-                        <aside class="single_sidebar_widget author_widget">
-                            <img class="author_img rounded-circle" src="{{ asset('themes/img/blog/author.png') }}"
-                                alt="">
-                            <h4>Charlie Barber</h4>
-                            <p>Senior blog writer</p>
-                            <div class="social_icon">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-github"></i></a>
-                                <a href="#"><i class="fa fa-behance"></i></a>
-                            </div>
-                            <p>Boot camps have its supporters andit sdetractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get. Boot camps have itssuppor
-                                ters andits detractors.</p>
-                            <div class="br"></div>
-                        </aside>
-                        
-                        <aside class="single_sidebar_widget ads_widget">
-                            <a href="#"><img class="img-fluid" src="img/blog/add.jpg" alt=""></a>
-                            <div class="br"></div>
-                        </aside>
+                    <div class="col-lg-12 col-md-12 blog_details">
+                        <h2>Roditri Mobil</h2>
+                        <p class="excert">
+                            Roditri Mobil adalah platform jual beli mobil bekas terpercaya yang hadir untuk memudahkan
+                            Anda menemukan mobil impian dengan harga yang bersahabat dan proses yang transparan. Kami
+                            menghadirkan pengalaman jual beli mobil yang aman, cepat, dan tanpa ribet, baik untuk
+                            pembeli maupun penjual.
+                        </p>
+                        <p>
+                            Dengan tim yang berpengalaman di bidang otomotif, kami siap membantu Anda dalam setiap
+                            langkah—mulai dari memilih unit mobil terbaik, inspeksi kendaraan, pengurusan dokumen,
+                            hingga layanan kredit dan tukar tambah.
+                        </p>
+                        <p>
+                            Kami percaya bahwa setiap orang berhak memiliki kendaraan yang nyaman dan sesuai kebutuhan,
+                            tanpa harus khawatir soal kualitas maupun proses transaksi. Karena itu, setiap unit mobil
+                            yang kami jual sudah melalui proses seleksi dan pemeriksaan ketat.
+                        </p>
                     </div>
                 </div>
             </div>
+            <div class="col-lg-4">
+                <div class="blog_right_sidebar">
+
+                    <aside class="single_sidebar_widget author_widget">
+                        <img class="author_img rounded-circle" src="{{ asset('themes/img/blog/author.png') }}" alt="">
+                        <h4>Tri Brothers</h4>
+                        <p>Owner of Roditri Mobil</p>
+                        <div class="social_icon">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                        <p>Roditri Mobil lahir dari semangat dan kolaborasi tiga saudara yang memiliki kecintaan
+                            mendalam terhadap dunia otomotif. Berangkat dari pengalaman pribadi dalam jual beli
+                            kendaraan serta keinginan untuk menciptakan layanan yang jujur dan terpercaya, lahirlah
+                            brand "Roditri Mobil", yang merupakan singkatan dari "Roda Tiga Bersaudara" atau dikenal
+                            juga dengan sebutan Tri Brother.</p>
+                    </aside>
+                </div>
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 @endsection
