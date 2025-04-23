6eabe11cbbd9dd2193d57710330f5b226b219750
ZhilanMuhammad <zhilanmuhammad11@gmail.com>
2025-04-23 20:55:10 +0700

    💄 update tentang

--- resources/views/page/tentang/index.blade.php
+++ resources/views/page/tentang/index.blade.php
@@ -27,18 +27,18 @@
 
                     <div class="col-lg-12 col-md-12 blog_details">
                         <h2>Roditri Mobil</h2>
-                        <p class="excert">
+                        <p class="excert text-justify">
                             Roditri Mobil adalah platform jual beli mobil bekas terpercaya yang hadir untuk memudahkan
                             Anda menemukan mobil impian dengan harga yang bersahabat dan proses yang transparan. Kami
                             menghadirkan pengalaman jual beli mobil yang aman, cepat, dan tanpa ribet, baik untuk
                             pembeli maupun penjual.
                         </p>
-                        <p>
+                        <p class="text-justify">
                             Dengan tim yang berpengalaman di bidang otomotif, kami siap membantu Anda dalam setiap
                             langkah—mulai dari memilih unit mobil terbaik, inspeksi kendaraan, pengurusan dokumen,
                             hingga layanan kredit dan tukar tambah.
                         </p>
-                        <p>
+                        <p class="text-justify">
                             Kami percaya bahwa setiap orang berhak memiliki kendaraan yang nyaman dan sesuai kebutuhan,
                             tanpa harus khawatir soal kualitas maupun proses transaksi. Karena itu, setiap unit mobil
                             yang kami jual sudah melalui proses seleksi dan pemeriksaan ketat.
@@ -49,30 +49,20 @@
             <div class="col-lg-4">
                 <div class="blog_right_sidebar">
 
-                        <aside class="single_sidebar_widget author_widget">
-                            <img class="author_img rounded-circle" src="{{ asset('themes/img/blog/author.png') }}"
-                                alt="">
-                            <h4>Charlie Barber</h4>
-                            <p>Senior blog writer</p>
-                            <div class="social_icon">
-                                <a href="#"><i class="fa fa-facebook"></i></a>
-                                <a href="#"><i class="fa fa-twitter"></i></a>
-                                <a href="#"><i class="fa fa-github"></i></a>
-                                <a href="#"><i class="fa fa-behance"></i></a>
-                            </div>
-                            <p>Boot camps have its supporters andit sdetractors. Some people do not understand why you
-                                should have to spend money on boot camp when you can get. Boot camps have itssuppor
-                                ters andits detractors.</p>
-                            <div class="br"></div>
-                        </aside>
-
-                        <aside class="single_sidebar_widget ads_widget">
-                            <a href="#"><img class="img-fluid" src="img/blog/add.jpg" alt=""></a>
-                            <div class="br"></div>
-                        </aside>
-                    </div>
+                    <aside class="single_sidebar_widget author_widget">
+                        <img class="author_img rounded-circle" src="{{ asset('themes/img/blog/author.png') }}" alt="">
+                        <h4>Tri Brothers</h4>
+                        <p>Owner of Roditri Mobil</p>
+                        <div class="social_icon"></div>
+                        <p class="text-justify">Roditri Mobil lahir dari semangat dan kolaborasi tiga saudara yang
+                            memiliki kecintaan
+                            mendalam terhadap dunia otomotif. Berangkat dari pengalaman pribadi dalam jual beli
+                            kendaraan serta keinginan untuk menciptakan layanan yang jujur dan terpercaya, lahirlah
+                            brand "Roditri Mobil", yang merupakan singkatan dari "Roda Tiga Bersaudara" atau dikenal
+                            juga dengan sebutan Tri Brother.</p>
+                    </aside>
                 </div>
             </div>
         </div>
-    </section>
+</section>
 @endsection
